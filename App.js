7f3785f97aa316a837d4f2d3e91ac46b3a819f8f
Luyang Liu <luyangl16@gmail.com>
2022-07-16 12:18:15 +1000

    Made react app work on ios and work on landing page.
    On branch master
    Changes to be committed:
    	new file:   .log/ti-13477.log
    	new file:   .log/tsserver.log
    	modified:   App.js
    	new file:   Story Board.fig
    	new file:   images/logo.png

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component, useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Logo from "./images/logo.png";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {profile: {
+            firstname: "Mike",
+            lastname: "Bob",
+            age: "5",
+            dob: "12-12-2018"
+        },
+                     };
+
+    }
+
+    render() {
+        return (
+                <View style={main.container}>
+                <Image style={imageStyles.stretch} source={ require('./images/logo.png') } />
+                <View style={nameContainer.container}>
+                <Text>Welcome {this.state.profile.firstname}</Text>
+                </View>
+                <StatusBar style="auto" />
+                </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+const main = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+const nameContainer = StyleSheet.create({
+    container: {
+        backgroundColor: '#8ED8F3',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 220,
+        height: 220,
+    },
+});
+
+
+const imageStyles = StyleSheet.create({
+    container: {
+        paddingTop: 50,
+    },
+    stretch: {
+        width: 220,
+        height: 200,
+        resizeMode: 'stretch',
+    },
 });
