a1a6eaa577a72844e9598bf676f32e429036224e
Luyang Liu <luyangl16@gmail.com>
2022-07-17 10:13:39 +1000

    Added icons and home button in header
    Changes to be committed:
    	new file:   aesop-project/components/buttons.js
    	modified:   aesop-project/components/header.js
    	new file:   aesop-project/components/style/buttons.js
    	modified:   aesop-project/components/style/header.js
    	new file:   aesop-project/package-lock.json
    	modified:   aesop-project/package.json
    	modified:   aesop-project/yarn.lock

---
+++ aesop-project/components/buttons.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AiTwotoneHome } from "react-icons/si";
+import { Component, useEffect } from 'react';
+import style from "./style/header.js";
+
+class HomeButton extends Component {
+
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+export default HomeButton;
