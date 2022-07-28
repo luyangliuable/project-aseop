fceef9f05c8e1813977043855a290961d1ef99a8
Luyang Liu <luyangl16@gmail.com>
2022-07-28 11:02:28 +1000

    Added profile placeholder and also added structure to prpfile screen.

--- aesop-project/components/style/main.js
+++ aesop-project/components/style/main.js
@@ -1,56 +1,66 @@
 import { StyleSheet, Dimensions, View, Image } from 'react-native';
 
 const main = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-    center_container: {
-        flex: 1,
-        backgroundColor: '#1e90ff',
-        position: "absolute",
-        width: "80%",
-        top: "25%",
-        height: "50%",
-        flexDirection: "column",
-        left: "10%",
-        alignItems: 'center',
-        padding: 10,
-        borderRadius: 10,
-        justifyContent: 'flex-start',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  outer_container: {
+    position: "absolute",
+    top: 20,
+    width: "100%",
+    height: "90%",
+    backgroundColor: "#EEE",
+    display: "flex",
+    justContent: "center",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  center_container: {
+    flex: 1,
+    backgroundColor: '#1e90ff',
+    position: "absolute",
+    width: "80%",
+    top: "25%",
+    height: "50%",
+    flexDirection: "column",
+    left: "10%",
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 10,
+    justifyContent: 'flex-start',
+  }
 });
 
 const nameContainer = StyleSheet.create({
-    container: {
-        backgroundColor: '#8ED8F3',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '50%',
-        height: "20%",
-    },
-    shadowProp: {
-        shadowColor: '#171717',
-        shadowOffset: { width: 2, height: 4 },
-        shadowOpacity: 0.2,
-        shadowRadius: 3,
-    },
+  container: {
+    backgroundColor: '#8ED8F3',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '50%',
+    height: "20%",
+  },
+  shadowProp: {
+    shadowColor: '#171717',
+    shadowOffset: { width: 2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
 });
 
 
 const imageStyles = StyleSheet.create({
-    container: {
-        paddingTop: 50,
-    },
-    stretch: {
-        width: '85%',
-        height: Dimensions.get('window').width*0.8,
-        resizeMode: 'stretch',
-    },
+  container: {
+    paddingTop: 50,
+  },
+  stretch: {
+    width: '85%',
+    height: Dimensions.get('window').width * 0.8,
+    resizeMode: 'stretch',
+  },
 });
 
 
-export default {main, nameContainer, imageStyles};
+export default { main, nameContainer, imageStyles };
